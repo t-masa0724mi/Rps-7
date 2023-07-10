06da8fd3a4622ce677d4bd10514737e8ded6ea08
masa950724mi <s100.iizuka.masami@gmail.com>
2023-07-10 16:18:05 +0900

    Initial Commit

---
+++ RPS-7/RPS_7App.swift
@@ -0,0 +1,17 @@
+//
+//  RPS_7App.swift
+//  RPS-7
+//
+//  Created by 飯塚政美 on 2023/07/10.
+//
+
+import SwiftUI
+
+@main
+struct RPS_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
