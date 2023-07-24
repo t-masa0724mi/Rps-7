be5bfd3957fa96ad4c458f4570ea25fb692118ec
masa950724mi <s100.iizuka.masami@gmail.com>
2023-07-24 13:35:22 +0900

    APIの修正と判定の修正

--- RPS-7/HTTPClient.swift
+++ RPS-7/HTTPClient.swift
@@ -1,21 +1,22 @@
 import Foundation
 
 class HTTPClient: ObservableObject {
-    @Published var computer: [Computer] = []
+    @Published var computer: Computer?
     
     func load() {
-        guard let url = URL(string: "http://52.195.82.85:5000/janken") else { return }
+        let jankenUrl = "http://52.195.82.85:5000/janken"
+        guard let url = URL(string: jankenUrl) else { return }
+        let request = URLRequest(url: url)
         
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data, error == nil else { return}
-            
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else { return }
+            let decoder = JSONDecoder()
+            guard let response = try? decoder.decode(Computer.self, from: data) else { return }
             DispatchQueue.main.async {
-                guard let response = try? JSONDecoder().decode([Computer].self, from: data) else {
-                    return print("hoge")
-                }
                 self.computer = response
             }
-        }.resume()
+        }
+        task.resume()
     }
 }
 
